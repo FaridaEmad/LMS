3e8d026fee0cd80a7b01548c64e076364b517d7d
FaridaEmad <101283196+FaridaEmad@users.noreply.github.com>
2023-05-07 21:18:06 +0300

    test

--- View/prof_dash.php
+++ View/prof_dash.php
@@ -118,7 +118,7 @@
             <div class="container-fluid pt-4 px-4">
                 <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
                     <div class="col-md-6 text-center">
-                        <h3>Prof page</h3>
+                        <h3>Profffff page</h3>
                     </div>
                 </div>
             </div>
