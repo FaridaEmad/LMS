ce7e9b64e535fae4a0bb17cbefb2bfedb05724f1
SamaaRabeaa <rabeaaasamaa607@gmail.com>
2023-05-08 11:21:25 +0200

    SE

--- View/prof_dash.php
+++ View/prof_dash.php
@@ -59,17 +59,7 @@
                             <a href="prof_viewExam.php" class="dropdown-item">View all exams </a>
                         </div>
 
-                        <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown"><i class="far fa-file-alt me-2"></i>Grades</a>
-                        <div class="dropdown-menu bg-transparent border-0">
-                            <a href="addgrade.php" class="dropdown-item">Add Grade</a>
-                            <a href="prof_viewgrade.php" class="dropdown-item">View  Grade</a>
-                         
-                        </div>
-
-
-                    </div>
-
+                  
                    
                     <a href="prof-viewgrade.php" class="nav-item nav-link "><i class="far fa-file-alt me-2"></i>Grade</a>
                     <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widget</a>
